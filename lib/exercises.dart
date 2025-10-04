1312aae3d6da09b18a21c56837b5b763739080ed
Aahwaan Khullar <aahwaankhullar@gmail.com>
2025-10-04 09:58:47 +0530

    changes

--- lib/exercises.dart
+++ lib/exercises.dart
@@ -1,5 +1,6 @@
 import 'package:downsyndromeapp/Exercises.dart';
 import 'package:downsyndromeapp/bodypartexercise.dart';
+import 'package:downsyndromeapp/danceexercises.dart';
 import 'package:downsyndromeapp/navbar.dart';
 import 'package:easy_rich_text/easy_rich_text.dart';
 import 'package:flutter/material.dart';
@@ -189,9 +190,13 @@ class _ExercisesState extends State<Exercises> {
               const SizedBox(height: 15),
 
               GestureDetector(
-                onTap: (){
+                onTap: ()async {
+                  print("pressed");
+                  // final data=User(nickname: nicknamecontroller.text, avatar: avatar, syndrome: syndrome)
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>DanceExerciseApp()));
 
                 },
+
                 child: Container(
                   padding: EdgeInsets.all(20),
                   decoration: BoxDecoration(
