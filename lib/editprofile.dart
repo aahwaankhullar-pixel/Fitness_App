3aa63576010ec2992e9d67690a886a7c5c2b3edb
janhavikarande07 <janhavi.karande2001@gmail.com>
2025-10-04 10:52:00 +0530

    exercise change

--- lib/editprofile.dart
+++ lib/editprofile.dart
@@ -42,41 +42,51 @@ class _EditProfileState extends State<EditProfile> {
       
       body:
         Container(
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              SizedBox(height: 10,),
               Container(
-                child: Image.asset(avatar,width: 200,),
-                
+                alignment: Alignment.center,
+                child: CircleAvatar(
+                  radius: 60,
+                  backgroundImage: AssetImage(avatar),
+                ),
               ),
+              SizedBox(height: 20,),
+              Text("Avatars", style: TextStyle(fontWeight: FontWeight.w700, fontSize: 16),),
+              SizedBox(height: 10,),
               SingleChildScrollView(
                 scrollDirection: Axis.horizontal,
                 child: Row(
                   children: [
                     for(dynamic avatar in avatars)
-                      
                       GestureDetector(
                         onTap: (){},
                         child: Container(
-                          child: CircleAvatar(
-                            radius: 30,
-                            backgroundImage: AssetImage(avatar["asset"]),
-                            
-                            
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            shape: BoxShape.circle,
+                            border: Border.all(),
                           ),
-                          
-                          
+                          margin: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+                          padding: EdgeInsets.all(2),
+                          child: CircleAvatar(
+                            radius: 40,
+                            child: Image.asset(
+                              avatar["asset"],
+                              fit: BoxFit.contain,
+                            ),
+                          ),
                         ),
-                      )
-                    
-                    
+                      ),
+                    SizedBox(width: 10,),
                   ],
-                  
-                  
                 ),
-                
               ),
-              
-              
+
+              SizedBox(height: 20,),
               TextFormField(
                 controller: nicknamecontroller,
                 decoration: InputDecoration(
@@ -97,17 +107,10 @@ class _EditProfileState extends State<EditProfile> {
                     borderRadius: BorderRadius.all(Radius.circular(10)),
                   ),
                 ),
-              ),              
-              
-              
+              ),
             ],
-            
-            
           ),
-          
-          
         )
-
     );
   }
 }
